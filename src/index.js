6cc09c0774f97f3e13218af90dc1f4fd4b69cab6
Alexandr <you@example.com>
2022-07-09 15:00:33 +0300

    add files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import './styles/style.css'; //import custom .css
+import './styles/style.scss'; //import custom .scss
+import logo from './images/logo.png' //custom pictur
+
+console.log('webpack starting');
+document.querySelector('.img img').src = logo;
---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+
+  entry: {
+    main: './src/index.js'
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  devServer: { //plugin webpack-dev-server
+    port: 3000
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, './src/index.html'),
+        filename: 'index.html'
+    }), 
+    new CleanWebpackPlugin()
+  ],
+
+  module: {
+    rules:[
+        {
+            test: /\.(png|jpe?g|gif)$/i,
+            loader: 'file-loader'
+        },
+        {
+            test: /\.css$/i,
+            use: ["style-loader", "css-loader"],
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use: ["style-loader", "css-loader", "sass-loader"],
+        },
+    ]
+}
+}
